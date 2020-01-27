6909f53b1573fb41b2dba76452fe9707ce14d572
Andrew <atsai12@gmail.com>
2020-01-27 01:15:45 -0800

    Set blueprint and finished first draft of checks and set. See comment for more details

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,6 +1,7 @@
 import Log from "../Util";
 import {IInsightFacade, InsightDataset, InsightDatasetKind} from "./IInsightFacade";
 import {InsightError, NotFoundError} from "./IInsightFacade";
+import PerformQueryHelper from "./PerformQueryHelper";
 
 /**
  * This is the main programmatic entry point for the project.
@@ -8,6 +9,8 @@ import {InsightError, NotFoundError} from "./IInsightFacade";
  *
  */
 export default class InsightFacade implements IInsightFacade {
+
+    private performQueryHelper: PerformQueryHelper;
 
     constructor() {
         Log.trace("InsightFacadeImpl::init()");
@@ -22,11 +25,60 @@ export default class InsightFacade implements IInsightFacade {
     }
 
     public performQuery(query: any): Promise <any[]> {
-        // Adding this line to make sure I can commit to the new performQuery branch and reflect on the github graph
-        return Promise.reject("Not implemented.");
+        let dataSetIDToUse = null;
+        let datasetToUse = null;
+        return Promise.reject("Not implemented."); // comment out
+
+        /**
+         * Step1: Check the grammar and semantics
+         * Semantics: ORDER's key (type string) is the column name to sort on and must be in COLUMNS array
+         */
+        if (this.performQueryHelper.inputQueryIsValid(query)) {
+            /**
+             * Step2: Set the dataset
+             * 1. Look in datasets, which is a list of Dataset
+             * 2. If we find the dataset with the id we want, set datasetToUse to be that dataset
+             * 3. Otherwise do a promise reject because we can't find the dataset
+             */
+            // Dataset class does exist, just need to merge with Ben's commit
+
+            // dataSetIDToUse = "courses";
+            // indexOfDataset = datasets.indexOf(dataSetIDToUse);
+            // if (indexOfDataset === -1) { //indexOf returns -1 if target value is not found
+            //     Promise.reject("dataset not found");
+            // } else {
+            //     datasetToUse = datasets[indexOfDataset];
+            // }
+
+            /**
+             * Step3: Now that the dataset is loaded, the query keys can be validated against the dataset
+             * Semantic checks in order:
+             * Check if the first key in COLUMNS is loaded
+             * Check if the other keys are different from COLUMNS (if so, then cannot reference more than one dataset)
+             * Check if query keys are one of the 8 keys we accept
+             */
+
+             // if (this.performQueryHelper.inputKeysAreValid(query, datasetToUse)) {
+             //
+             //    /**
+             //     * Step4: Store the query in a structure such that the query can be performed.
+             //     * The hierarchy should match the incoming JSON
+             //     */
+             //    // this.performQueryHelper.storeQuery(query);
+             //
+             //    /**
+             //     * Step5: Run the query
+             //     */
+             //    // this.performQueryHelper.runQuery(query, dataSet);
+             // }
+
+
+        }
+
     }
 
     public listDatasets(): Promise<InsightDataset[]> {
         return Promise.reject("Not implemented.");
+
     }
 }
---
+++ src/controller/PerformQueryHelper.ts
@@ -0,0 +1,156 @@
+export default class PerformQueryHelper {
+
+    public errorMessage: string;
+
+
+    constructor() {
+        this.errorMessage = "";
+    }
+    // Helper functions
+
+    // check if input query is valid.
+    public inputQueryIsValid(query: any): boolean {
+
+        // return true if the query has a body and OPTIONS
+        if (this.hasBodyAndOptions(query)) {
+            // return true if the query has valid OPTIONS grammar
+            return this.hasValidOptionsGrammar(query);
+        } else {
+            return false;
+        }
+    }
+
+        // inputQueryIsValid helper method
+        // true if input query's first key is "WHERE" and second key is "OPTIONS"
+        private hasBodyAndOptions(query: any): boolean {
+            // Object.keys returns an array of the given object's property names.
+            // keys takes the object as a parameter
+            if (Object.keys(query)[0] !== "WHERE") {
+                this.errorMessage = "Missing WHERE";
+                return false;
+            } else if (Object.keys(query)[1] !== "OPTIONS") {
+                this.errorMessage = "Missing OPTIONS";
+                return false;
+            } else if (Object.keys(query)[0] === "WHERE" && Object.keys(query)[1] === "OPTIONS") {
+                return true;
+            }
+        }
+
+        /** inputQueryIsValid helper method
+         * true if OPTIONS grammar is valid:
+         *  - Must have ONE "COLUMNS"
+         *      - COLUMNS can have identical child keys
+         *  - See hasValidOptionsGrammar helper method for ORDER spec
+         */
+        private hasValidOptionsGrammar(query: any) {
+            // OPTIONS must have one or two keys
+            if (Object.keys(query["OPTIONS"]).length === 1 || Object.keys(query["OPTIONS"]).length === 2) {
+                // The first key should be "COLUMNS"
+                if (Object.keys(query["OPTIONS"])[0] === "COLUMNS") {
+                    // Columns must not be empty
+                    if (Object.keys(query["OPTIONS"]).length === 0) {
+                        this.errorMessage = "COLUMNS must be a non-empty array";
+                        return false;
+                    } else {
+                        // if there is a second key
+                        if (Object.keys(query["OPTIONS"]).length === 2) {
+                            // if the second key is a valid ORDER, return true
+                            return this.orderIsValid(query);
+                        } else { // there is no second key
+                            return true;
+                        }
+                    }
+                } else {
+                    this.errorMessage = "OPTIONS missing COLUMNS";
+                    return false;
+                }
+            } else {
+                this.errorMessage = "OPTIONS must have one or two keys";
+                return false;
+            }
+        }
+
+            /** hasValidOptionsGrammar helper method
+             *  ORDER is optional, but can only have one ORDER if exists
+             *    - ORDER must have exactly one child key
+             *    - the child key must be one of the column's child keys
+             */
+            private orderIsValid(query: any) {
+
+
+                // the order key must be called "ORDER"
+                if (Object.keys(query["OPTIONS"])[1] === "ORDER") {
+
+                    let order = Object.keys(query["OPTIONS"])[1]; // sets order to be the second key in OPTIONS (ORDER)
+
+                    // the order key must have exactly one field
+                    if (order.length === 1) {
+
+                        let orderField = order[0];
+                        let columns = query["OPTIONS"]["COLUMNS"]; // sets columns to be the list of fields in "COLUMNS"
+
+                        // the order field must be in COLUMNS
+                        if (columns.includes(orderField)) {
+                            return true;
+                        } else {
+                            this.errorMessage = "ORDER key must be in COLUMNS";
+                            return false;
+                        }
+                    } else {
+                        this.errorMessage = "Invalid query string (ORDER must have exactly one field";
+                        return false;
+                    }
+                } else {
+                    this.errorMessage = "Invalid keys in OPTIONS (cannot find ORDER)";
+                    return false;
+                }
+
+            }
+
+    /**
+     * inputKeysAreValid returns true if three semantic checks are passed:
+     * Semantic checks in order:
+     * Check if the first key in COLUMNS is loaded
+     * Check if the other keys are different from COLUMNS (if so, then cannot reference more than one dataset)
+     * Check if query keys are one of the 8 keys we accept
+     */
+    // TODO: Comment in
+    // public inputKeysAreValid(query: any, datasetToUse: Dataset): boolean {
+    //     // Check if first key in COLUMNS is loaded
+    //     if (this.keyIsLoaded(query, datasetToUse)) {
+    //         // Check if only one dataset is referenced
+    //         if (this.onlyOneDatasetReferenced(query, datasetToUse)) {
+    //             // Check if query keys are one of the 8 keys that are allowed
+    //             if (this.allKeysAllowed(query)) {
+    //                 return true;
+    //             } else {
+    //                 // TODO: Find out how to fill in the blanks
+    //                 this.errorMessage = "Invalid key ___(ex. courses_session)___ in ___(ex. COLUMNS || GT)___"
+    //                 return false;
+    //             }
+    //         } else {
+    //             this.errorMessage = "Cannot query more than one dataset";
+    //             return false;
+    //         }
+    //     } else {
+    //         this.errorMessage = "Referenced dataset " + "'" + "[insert datasetID]" + "'" +  " not added yet";
+    //         return false;
+    //     }
+    // }
+
+        /**
+         * Helper function for inputKeysAreValid
+         * Check if key is loaded
+         */
+
+        // TODO: comment in
+        // private keyIsLoaded(query: any, datasetToUse: Dataset): boolean {
+        //     // get the first key in COLUMNS
+        //     let key = query["OPTIONS"]["COLUMNS"][0];
+        //     return key === Dataset[0];
+        // }
+
+        private onlyOneDatasetReferenced(query: any, datasetID: string): boolean {
+            return false;
+        }
+}
