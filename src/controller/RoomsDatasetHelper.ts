458ff9429587706d0c2decea610e0d89da2ecfc1
benantho <benantho@students.cs.ubc.ca>
2020-02-17 13:15:43 -0800

    got everything implemented but stupid async is making it not work

--- src/controller/RoomsDatasetHelper.ts
+++ src/controller/RoomsDatasetHelper.ts
@@ -2,6 +2,8 @@ import Log from "../Util";
 import * as JSZip from "jszip";
 import * as parse5 from "parse5";
 import Room from "./Room";
+import * as http from "http";
+import {IncomingMessage} from "http";
 
 export default class RoomsDatasetHelper {
 
@@ -19,11 +21,10 @@ export default class RoomsDatasetHelper {
             try {
                 jsZip.loadAsync(this.content, {base64: true})
                     .then((zip: JSZip) => {
-                        zip
-                            .file(fileName)
+                        zip.file("rooms" + fileName)
                             .async("text")
-                            .then((indexhtmAsString: string) => {
-                                resolve(parse5.parse(indexhtmAsString));
+                            .then((htmlAsString: string) => {
+                                resolve(parse5.parse(htmlAsString));
                             });
                     });
             } catch (err) {
@@ -60,14 +61,6 @@ export default class RoomsDatasetHelper {
         }
     }
 
-    // private tableSearcher(parent: any) {
-    //     for (let node of parent["childNodes"]) {
-    //         if (node["nodeName"] === "tbody") {
-    //             return node;
-    //         }
-    //     }
-    // }
-
     private buildingLevelTableSearch(parent: any) {
         let tbody: any;
         for (let node of parent["childNodes"]) {
@@ -88,25 +81,61 @@ export default class RoomsDatasetHelper {
         this.trSearcher(node, room);
         Log.trace("found a building in index.htm, got its shortname, fullname, and address");
         if (room.address !== null) {
-            // do the lat/lon stuff here
-            // try catch this so that if it results in an error we go no further with this room, it's invalid
+            this.getLatLon(room);
+            // if this thing is invalid it's gonna throw an error, should just let that ride all the way back up to top
+            // and return insight error from there
+            // todo: this thing should be wrapped in a promise, it's async
         }
         if (room.href !== null) {
-            this.parseRoomsZip(room.href)
+            this.parseRoomsZip(room.href.substring(1))
                 .then((roomHTML: any) => {
                     this.roomBuilder(roomHTML, room);
                 });
         }
     }
 
+    private getLatLon(room: Room) {
+        let encoded: string = encodeURI(room.address);
+        let url: string = "http://cs310.students.cs.ubc.ca:11316/api/v1/project_team097/" + encoded;
+        http.get(url, (res: IncomingMessage) => {
+            // callback function taken from https://nodejs.org/api/http.html#http_http_get_options_callback
+            res.setEncoding("utf8");
+            let rawData: any = "";
+            res.on("data", (chunk) => {
+                rawData += chunk;
+            });
+            res.on("end", () => {
+                try {
+                    const parsedData = JSON.parse(rawData);
+                    if (parsedData["error"] !== undefined) {
+                        throw new Error("the lat/lon request resulted in an error");
+                    }
+                    room.lat = parsedData["lat"];
+                    room.lon = parsedData["lon"];
+                } catch (e) {
+                    Log.error(e.message);
+                }
+            });
+        }).on("error", (e) => {
+            Log.error(`Got error: ${e.message}`);
+        });
+    }
+
     private roomBuilder(node: any, room: Room) {
         let tbody: any = this.findHTMLBody(node);
         this.roomLevelRecursion(tbody, room);
         Log.trace("room has been filled at this point, is ready to be pushed to rooms provided it's not empty");
-        // if the room's fields that should've been filled in at this point (number, seats, furniture, type)
-        // then the building has no rooms, can skip this (*** check to make sure that if u have a building with no rooms
-        // you'll just not get to this point, the recursion might catch it)
-        this.rooms.push(room);
+        if (!this.emptyBuilding(room)) {
+            room.name = room.shortname + " " + room.number;
+            this.rooms.push(room);
+        }
+    }
+
+    private emptyBuilding(room: Room) {
+        return room.type === undefined &&
+            room.furniture === undefined &&
+            room.seats === undefined &&
+            room.number === undefined;
     }
 
     private roomLevelRecursion(parent: any, room: Room) {
@@ -120,11 +149,9 @@ export default class RoomsDatasetHelper {
     }
 
     private trSearcher(node: any, room: Room) {
-        // if (node["nodeName"] === "tr") {
-            for (let child of node["childNodes"]) {
-                this.tdSearcher(child, room);
-            }
-        // }
+        for (let child of node["childNodes"]) {
+            this.tdSearcher(child, room);
+        }
     }
 
     private tdSearcher(node: any, room: Room) {
@@ -138,15 +165,15 @@ export default class RoomsDatasetHelper {
                 } else if (child["value"] === "views-field views-field-field-building-address") {
                     room.address = node["childNodes"][0]["value"].trim();
                 } else if (child["value"] === "views-field views-field-field-room-number") {
-                    room.number = node["childNodes"][0]["value"].trim();
+                    room.number = node["childNodes"][1]["childNodes"][0]["value"].trim();
                 } else if (child["value"] === "views-field views-field-field-room-capacity") {
                     room.seats = parseInt(node["childNodes"][0]["value"].trim(), 10);
                 } else if (child["value"] === "views-field views-field-field-room-furniture") {
                     room.furniture = node["childNodes"][0]["value"].trim();
                 } else if (child["value"] === "views-field views-field-field-room-type") {
                     room.type = node["childNodes"][0]["value"].trim();
-                } else if (child["value"] === "views-field views-field-field-room-type") {
-                    room.type = node["childNodes"][0]["value"].trim();
+                } else if (child["value"] === "views-field views-field-title") {
+                    room.fullname = node["childNodes"][1]["childNodes"][0]["value"].trim();
                 }
             }
         }
@@ -166,7 +193,7 @@ export default class RoomsDatasetHelper {
 
     public getAllRoomsMasterMethod(content: string) {
         this.content = content;
-        return this.parseRoomsZip("rooms/index.htm")
+        return this.parseRoomsZip("/index.htm")
             .then((indexhtm: any) => {
                 Log.trace("parsed index.htm into a JSON object");
                 let body: any = this.findHTMLBody(indexhtm);
