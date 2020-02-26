37be3db75aeb3bf47dbf0fa2afcde2c41d814289
Andrew <atsai12@gmail.com>
2020-02-25 23:41:19 -0800

    added modified files

--- src/controller/PQGeneralHelpers.ts
+++ src/controller/PQGeneralHelpers.ts
@@ -1,30 +1,22 @@
 export default class PQGeneralHelpers {
-    public listOfAcceptableFields: string[];
-    public acceptableStringFields: string[];
-    public acceptableNumberFields: string[];
-    public dataSetID: string = "courses"; // "courses" by default, will be changed in establishDataset
-    public static listOfAcceptableFields = [
-        "dept",
-        "id",
-        "avg",
-        "instructor",
-        "title",
-        "pass",
-        "fail",
-        "audit",
-        "uuid",
-        "year",
+    public static dataSetID: string = "courses"; // "courses" by default, will be changed in establishDataset
+    public static dataSetKind: string = "courses"; // "courses" by default, will be changed in establishDataset
+    public static listOfAcceptableFields = [ "dept", "id", "avg", "instructor", "title",
+        "pass", "fail", "audit", "uuid", "year", "fullname", "shortname", "number", "name",
+        "address", "lat", "lon", "seats", "type", "furniture", "href"
     ];
 
-    public static acceptableStringFields = ["dept", "id", "instructor", "title", "uuid"];
-    public static acceptableNumberFields = ["avg", "pass", "fail", "audit", "year"];
+    public static acceptableStringFields = ["dept", "id", "instructor", "title", "uuid",
+        "fullname", "shortname", "number", "name", "address", "type", "furniture", "href"];
+
+    public static acceptableNumberFields = ["avg", "pass", "fail", "audit", "year", "lat", "lon", "seats"];
 
     public setDataSetID(value: string) {
-        this.dataSetID = value;
+        PQGeneralHelpers.dataSetID = value;
     }
 
     public getDataSetID() {
-        return this.dataSetID;
+        return PQGeneralHelpers.dataSetID;
     }
 
     public keyIsValidColumnUnderscoreItem(key: any): any {
@@ -35,7 +27,7 @@ export default class PQGeneralHelpers {
 
         // Step 2: Check ID of id_field
         let keyID = key.substring(0, key.indexOf("_")); // ex. courses_avg -> courses
-        if (keyID !== this.dataSetID) {
+        if (keyID !== PQGeneralHelpers.dataSetID) {
             return "COLUMNS cannot reference multiple datasets or dataset not loaded yet";
         }
 
@@ -46,6 +38,79 @@ export default class PQGeneralHelpers {
         }
 
         return true;
+    }
+
+    /**
+     * Translates query key_value format to JSON value format
+     * ex. Query has wants to know "courses_avg", so we have to search
+     * each section's "Avg" key.
+     */
+    public static translate(queryKey: string): string {
+        let id = PQGeneralHelpers.dataSetID; // ex. "courses" || "rooms"
+        if (PQGeneralHelpers.dataSetKind === "courses") {
+            return this.switchForCourses(queryKey, id);
+        }
+        if (PQGeneralHelpers.dataSetKind === "rooms") {
+            return this.switchForRooms(queryKey, id);
+        }
+    }
+
+    private static switchForCourses(queryKey: string, id: string) {
+        switch (queryKey) {
+            // STRINGS
+            case id.concat("_", "dept"):
+                return "Subject";
+            case id.concat("_", "id"):
+                return "Course";
+            case id.concat("_", "instructor"):
+                return "Professor";
+            case id.concat("_", "title"):
+                return "Title";
+            case id.concat("_", "uuid"):
+                return "id";
+
+            // NUMBERS
+            case id.concat("_", "avg"):
+                return "Avg";
+            case id.concat("_", "pass"):
+                return "Pass";
+            case id.concat("_", "fail"):
+                return "Fail";
+            case id.concat("_", "audit"):
+                return "Audit";
+            case id.concat("_", "year"):
+                return "Year";
+        }
+    }
+
+    private static switchForRooms(queryKey: string, id: string) {
+        switch (queryKey) {
+            // STRINGS
+            case id.concat("_", "fullname"):
+                return "fullname";
+            case id.concat("_", "shortname"):
+                return "shortname";
+            case id.concat("_", "number"):
+                return "number";
+            case id.concat("_", "name"):
+                return "name";
+            case id.concat("_", "address"):
+                return "address";
+            case id.concat("_", "type"):
+                return "type";
+            case id.concat("_", "furniture"):
+                return "furniture";
+            case id.concat("_", "href"):
+                return "href";
+
+            // NUMBERS
+            case id.concat("_", "lat"):
+                return "lat";
+            case id.concat("_", "lon"):
+                return "lon";
+            case id.concat("_", "seats"):
+                return "seats";
+        }
     }
 
 }
